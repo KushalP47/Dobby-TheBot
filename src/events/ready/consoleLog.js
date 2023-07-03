50c28231201cc92dcffc4d50e9c848d9573ec424
Kushal Patel <kushal300603@gmail.com>
2023-07-04 00:27:54 +0530

    debugger removed

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -3,17 +3,17 @@ const Level = require('../../models/Level');
 module.exports = (client) => {
     console.log(`${client.user.username} is at your service`);
 
-    const query = {
-        username: "potterhead_vishwa",
-        role: "wmc-mentor",
-    }
+    // const query = {
+    //     username: "potterhead_vishwa",
+    //     role: "wmc-mentor",
+    // }
 
-    const level = Level.findOne(query)
-    if(level){
-        console.log(level);
-    }
-    else{
-        console.log("Query didn't found");
-    }
-    return;
+    // const level = Level.findOne(query)
+    // if(level){
+    //     console.log(level);
+    // }
+    // else{
+    //     console.log("Query didn't found");
+    // }
+    // return;
 };
