79f8375bc5ab70f82a00ad79a669f048a2164691
Kushal Patel <kushal300603@gmail.com>
2023-07-04 14:50:05 +0530

    changed port

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@ require('dotenv').config()
 const { Client, IntentsBitField, EmbedBuilder, Embed, ActivityType,  } = require("discord.js");
 const mongoose = require('mongoose');
 const eventHandler = require('./handlers/eventHandler');
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
 
 const client = new Client({
   intents: [
